aef15ec4962b7b5c1ac09976280c951db8063412
mackenzie <macmcclemont@hotmail.com>
2022-04-07 16:33:27 -0400

    styling changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = {}
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', './particlesjs-config.json', function () {
+window.particlesJS.load('particles-js', 'assets/particlesjs-config.json', function () {
     console.log('callback - particles.js config loaded');
 });
 
