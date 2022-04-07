6b77fbed43c4e52814df9ca09f2323c1bb372c9c
mackenzie <macmcclemont@hotmail.com>
2022-04-07 14:39:22 -0400

    reformatted my portfolio, added new styling to both the header and main sections, added some JS functionality to nav and project section

---
+++ app.js
@@ -0,0 +1,37 @@
+const app = {}
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './particlesjs-config.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+
+
+window.onscroll = () => {
+    let navBar = document.querySelector('.navContainer') 
+    console.log(navBar);
+    console.log(document.scrollingElement.scrollTop)
+    if(document.scrollingElement.scrollTop > 200) {
+        navBar.style.backgroundColor = '#051622'
+    } else if (document.scrollingElement.scrollTop == 0) {
+        navBar.style.backgroundColor = 'transparent'
+    }
+
+}
+
+app.projectImages = () => {
+    let imgElement = document.querySelector('.selectedImage')
+    let projectTitle = document.querySelectorAll('.titleText')
+
+    projectTitle.forEach( (project) => {
+        project.addEventListener('click', () => {
+            imgElement.src = `./assets/${project.textContent}.png`
+        })
+    })
+
+}
+
+app.init = () => {
+    app.projectImages();
+}
+
+app.init();
